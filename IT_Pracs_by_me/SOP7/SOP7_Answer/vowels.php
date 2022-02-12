591aca9a6eddf835c8c04305ad54cf866e461310
denzven <denzvenvadakkan@gmail.com>
2022-02-12 14:19:17 +0530

    Complete

--- IT_Pracs_by_me/SOP7/SOP7_Answer/vowels.php
+++ IT_Pracs_by_me/SOP7/SOP7_Answer/vowels.php
@@ -1,16 +1,16 @@
 <?php
 if(isset($_POST['submit'])){
-	$s = strtolower($_POST['str']);
-	count1($s);
+	$inputString = strtolower($_POST['str']);
+	getNumberOfVowels($inputString);
 }
 
-function count1($s){
-	$vowels = array('a','e','i','o','u');
-	$len1 = strlen($s);
-	$count = 0;
-	for($i=0;$i<$len1;$i++){
-		if(in_array($s[$i],$vowels)){
-			$count++;
+function getNumberOfVowels($inputString){
+	$Vowels = array('a','e','i','o','u');
+	$lengthOfString = strlen($inputString);
+	$numberOfVowels = 0;
+	for($i=0; $i<$lengthOfString; $i++){
+		if(in_array($inputString[$i],$Vowels)){
+			$numberOfVowels++;
 		}
 	}
 	echo"Number of vowels.$count";
